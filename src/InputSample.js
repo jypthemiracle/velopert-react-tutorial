8ab2a4543f99f7f68cad6ae91616026ffcf312a2
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-05-05 00:02:57 +0900

    feat: UseRef to manage specific DOM - to focus on name after reset

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,44 +1,52 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
-    const [inputs, setInputs] = useState({
-        name: '',
-        nickname: ''
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+  const nameInput = useRef();
+
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+
+  const onChange = e => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
     });
+  };
 
-    const nameInput = useRef();
-    
-    const {name, nickname} = inputs;
+  const onReset = () => {
+    setInputs({
+      name: '',
+      nickname: ''
+    });
+    nameInput.current.focus();
+  };
 
-    const onChange = (e) => {
-        const {value, name} = e.target;
-        setInputs({
-            ...inputs, // spread 문법 이용한 기존 inputs 객체 복사
-            [name]: value // name 키를 가진 값을 value로 설정
-        });
-    };
-
-    const onReset = () => {
-        setInputs({
-            name: '',
-            nickname: ''
-        });
-        // useRef() 를 사용하여 Ref 객체를 만들고,
-        nameInput.current.focus();
-    };
-
-    return (
-        <div>
-            {/* 이 객체를 우리가 선택하고 싶은 DOM에 ref 값으로 설정해주어야 합니다. */}
-            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput}/>
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
-            <button onClick={onReset}>초기화</button>
-            <div>
-                <b>값: </b>
-                {name} ({nickname})
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값: </b>
+        {name} ({nickname})
+      </div>
+    </div>
+  );
 }
 
 export default InputSample;
