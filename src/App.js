7aaabc7e31fd6f66a55064791f9af943863a1f4f
Jin Hyung Park <hophfg@yahoo.co.kr>
2021-05-05 00:54:16 +0900

    feat: modify the color of user

--- src/App.js
+++ src/App.js
@@ -53,6 +53,12 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   }
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user => user.id === id ? { ...user, active: !user.active} : user)
+    )
+  }
+
   return (
     <>
       <CreateUser
@@ -61,7 +67,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
